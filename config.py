16e2685f1fc2ba584ce7699663a014fa4ef6f84d
Alex Light <scialexlight@gmail.com>
2012-01-15 19:41:22 -0500

    added copyright notices

--- config.py
+++ config.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python
-
+# Copyright 2012, Alex Light.
+#
+# This file is part of Reddit background updater (RBU).
+#
+# RBU is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# RBU is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RBU.  If not, see <http://www.gnu.org/licenses/>.
+#
 #this file will parse the config files and the cli input for the application
 import os
 import argparse
--- loggers.py
+++ loggers.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python
+# Copyright 2012, Alex Light.
+#
+# This file is part of Reddit background updater (RBU).
+#
+# RBU is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# RBU is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RBU.  If not, see <http://www.gnu.org/licenses/>.
+#
 #this file is part of the Reddit Background Updater
 import sys
 try:
