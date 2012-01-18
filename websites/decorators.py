b26600035d49c5c9185e24449b8b46cd408fe5c7
Alex Light <scialexlight@gmail.com>
2012-01-18 12:54:16 -0500

    try tags, and copyright

--- websites/decorators.py
+++ websites/decorators.py
@@ -2,8 +2,17 @@
 #
 # This file is part of Reddit background updater (RBU).
 #
-#if __package__ == None:
-#    __package__ = 'reddit_wallpaper.websites.decorators'
+# RBU is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# RBU is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RBU.  If not, see <http://www.gnu.org/licenses/>.
+#
+
     
 import re
 from .._exceptions import Unsuitable
