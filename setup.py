30ea05ddde7fe92f41a39e36b0964f0d383c6031
Alex Light <scialexlight@gmail.com>
2012-01-20 13:49:41 -0500

    made a setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = 'reddit_wallpaper',
+      version = '0.4',
+      description = 'updates the wallpaper from reddit',
+      author = 'Alexander Light',
+      packages = ['reddit_wallpaper', 'reddit_wallpaper.websites'])
+
