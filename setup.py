e3e7e47fe8d3a996a0f91987b6530975b6311558
Alex Light <scialexlight@gmail.com>
2012-01-20 20:19:04 -0500

    added license to setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(name = 'reddit_wallpaper',
       version = '0.4',
       description = 'updates the wallpaper from reddit',
       author = 'Alexander Light',
-      packages = ['reddit_wallpaper', 'reddit_wallpaper.websites'])
+      packages = ['reddit_wallpaper', 'reddit_wallpaper.websites'],
+      license = "GPL")
 
