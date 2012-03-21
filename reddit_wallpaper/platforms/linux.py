8455148f2e512dd1751ca05e45de17f7a15dab30
Alex Light <scialexlight@gmail.com>
2012-03-21 10:43:46 -0400

    added in gsettings support hopefully

--- reddit_wallpaper/platforms/linux.py
+++ reddit_wallpaper/platforms/linux.py
@@ -13,23 +13,86 @@
 # along with RBU.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-import gconf
+try:
+    import gconf
+    _HAS_GCONF = True
+except ImportError:
+    _HAS_GCONF = False
+
+try:
+    import gio
+    _HAS_GIO = True
+except ImportError:
+    _HAS_GIO = False
+
 from .. import _exceptions
 from ..loggers import DEBUG, INFO, NOTICE, WARNING, ERROR, CRITICAL, ALERT, EMERGENCY
 
 _GCONF_KEY = '/desktop/gnome/background/picture_filename'#key to write new wallpaper to
+_GSETTINGS_SCHEMA = "org.gnome.desktop.background" #schema for gsettings
+_GSETTINGS_KEY = "picture-uri" #key for gsettings
+
 DEFAULT_SAVE_LOCATION = '~/.background_getter/@'
 
-def set_as_background(conf, file_location):
+def _log_succsess(conf, mod = None):
+    msg = 'changed the background{} succsessfully'
+    if mod is not None:
+        msg = msg.format(" ".join((" in",str(mod))))
+    else:
+        msg = msg.format("")
+    conf.logger(DEBUG, msg)
+
+def _log_failed(conf, mod = None):
+    msg = 'was unable to change the background, {}client reported failure of key setting.'
+    if mod is not None:
+        msg = msg.format(mod)
+    else:
+        msg = msg.format("")
+    conf.logger(ERROR, msg)
+
+def _gconf_set_as_background(conf, file_location):
     """Sets the background path to the given path"""
     client = gconf.client_get_default()
     worked = client.set_string(_GCONF_KEY, file_location)
     client.suggest_sync()
     if worked:
-        conf.logger(DEBUG, 'changed the background succsessfully')
+        _log_succsess(conf,"gconf")
     else:
-        conf.logger(ERROR, 'was unable to change the background, gconf client reported failure of key setting.')
+        _log_failed(conf,"gconf")
         raise _exceptions.Failed("could not set gconf key")
+    return
+
+def _gsettings_set_as_background(conf, file_location):
+    """Sets the background path to the given path in gsettings"""
+    settings = gio.gsettings.new(_GSETTINGS_SCHEMA)
+    worked = settings.set_string(_GSETTINGS_KEY,file_location)
+    # I do not think i need this sync command.
+    #settings.sync()
+    if worked:
+        _log_succsess(conf,"gsetting")
+    else:
+        _log_failed(conf,"gsettings")
+        raise _exceptions.Failed("could not set gsettings key")
+    return
+
+def set_as_background(conf, file_location):
+    thrown = True
+    if _HAS_GIO:
+        try:
+            _gsettings_set_as_background(conf, file_location)
+            thrown = False
+        except _exceptions.Failed as e:
+            thrown.append(e)
+            pass
+    if _HAS_GCONF:
+        try:
+            _gconf_set_as_background(conf, file_location):
+            thrown = False
+        except _exceptions.Failed as e:
+            thrown.append(e)
+            pass
+    if thrown:
+        raise _exceptions.Failed("was unable to set background using any backend")
     return
 
 __all__ = ['DEFAULT_SAVE_LOCATION', 'set_as_background']
