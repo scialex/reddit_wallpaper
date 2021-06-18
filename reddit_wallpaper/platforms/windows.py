7785da9125df88277e21861f8cea7ca5c295d02d
Alex Light <scialexlight@gmail.com>
2021-06-18 14:06:12 -0700

    add windows support

--- reddit_wallpaper/platforms/windows.py
+++ reddit_wallpaper/platforms/windows.py
@@ -13,40 +13,35 @@
 # along with RBU.  If not, see <http://www.gnu.org/licenses/>.
 #
 import os
-from os.path import dirname, join
-from ctypes import windll
-from PIL import Image
+# from os.path import dirname, join
+# from ctypes import windll
+# from PIL import Image
+import subprocess
 from .. import _exceptions
 from ..loggers import DEBUG, INFO, NOTICE, WARNING, ERROR, CRITICAL, ALERT, EMERGENCY
 
-SPI_SETDESKWALLPAPER = 20
-SPIF_UPDATEINIFILE = 1
-SPIF_SENDWININICHANGE = 2
 DEFAULT_SAVE_LOCATION = '~/_background_getter/@'
+POWERSHELL_COMMAND="set-wallpaper"
 
 def set_as_background(conf, file_location):
     """
-    Sets the background path to the given path. It does this by making a copy as a bitmap and 
-    setting the wallpaper to that. The bitmap is always stored at the file's location and named
-    current_wallpaper.bmp
-
-    This DOES NOT WORK ATM FOR UNKNOWN REASONS
     
     """
-    bmp_file_name = join(dirname(file_location), current_wallpaper.bmp)
-    fle = open(file_location, 'rb')
-    img = Image.open(fle)
-    outfile = open(bmp_file_name, 'w')
-    img.save(outfile, 'BMP')
-    outfile.close()
-    outfile.flush()
-    os.fsync(outfile.fileno())
-    result = windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, bmp_file_name, SPIF_SENDWININICHANGE | SPIF_UPDATEINIFILE)
-    if not result: # == 1
-        conf.logger(ERROR, "was unable to set the registry key to change the wallpaper")
-        raise _exceptions.Failed('was unable to set the registry key needed to change the wallpaper')
-    else:
-        conf.logger(INFO, "changed the wallpaper successfully")
-    return 
+    subprocess.run(['powershell', '-Command', '{} {}'.format(POWERSHELL_COMMAND, file_location)])
+    # bmp_file_name = join(dirname(file_location), current_wallpaper.bmp)
+    # fle = open(file_location, 'rb')
+    # img = Image.open(fle)
+    # outfile = open(bmp_file_name, 'w')
+    # img.save(outfile, 'BMP')
+    # outfile.close()
+    # outfile.flush()
+    # os.fsync(outfile.fileno())
+    # result = windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, bmp_file_name, SPIF_SENDWININICHANGE | SPIF_UPDATEINIFILE)
+    # if not result: # == 1
+    #     conf.logger(ERROR, "was unable to set the registry key to change the wallpaper")
+    #     raise _exceptions.Failed('was unable to set the registry key needed to change the wallpaper')
+    # else:
+    #     conf.logger(INFO, "changed the wallpaper successfully")
+    # return 
         
 __all__ = ['DEFAULT_SAVE_LOCATION', 'set_as_background']
